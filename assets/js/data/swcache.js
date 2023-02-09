c7d9d676ef4e86482a2e21445ace34c67c24dd73
ddtdt113 <41114834+ddtdt113@users.noreply.github.com>
2023-02-09 16:50:28 +0900

    test
    test

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -1,44 +1,56 @@
----
-layout: compress
-
-# The list to be cached by PWA
----
-
 const resource = [
     /* --- CSS --- */
-    '{{ "/assets/css/style.css" | relative_url }}',
+    '/ddtdt113.github.io/assets/css/style.css',
 
     /* --- PWA --- */
-    '{{ "/app.js" | relative_url }}',
-    '{{ "/sw.js" | relative_url }}',
+    '/ddtdt113.github.io/app.js',
+    '/ddtdt113.github.io/sw.js',
 
     /* --- HTML --- */
-    '{{ "/index.html" | relative_url }}',
-    '{{ "/404.html" | relative_url }}',
+    '/ddtdt113.github.io/index.html',
+    '/ddtdt113.github.io/404.html',
 
-    {% for tab in site.tabs %}
-        '{{ tab.url | relative_url }}',
-    {% endfor %}
+    
+        '/ddtdt113.github.io/categories/',
+    
+        '/ddtdt113.github.io/tags/',
+    
+        '/ddtdt113.github.io/archives/',
+    
+        '/ddtdt113.github.io/about/',
+    
 
     /* --- Favicons & compressed JS --- */
-    {% assign cache_list = site.static_files | where: 'swcache', true  %}
-    {% for file in cache_list %}
-        '{{ file.path | relative_url }}'{%- unless forloop.last -%},{%- endunless -%}
-    {% endfor %}
+    
+    
+        '/ddtdt113.github.io/assets/img/favicons/android-chrome-192x192.png',
+        '/ddtdt113.github.io/assets/img/favicons/android-chrome-512x512.png',
+        '/ddtdt113.github.io/assets/img/favicons/apple-touch-icon.png',
+        '/ddtdt113.github.io/assets/img/favicons/browserconfig.xml',
+        '/ddtdt113.github.io/assets/img/favicons/favicon-16x16.png',
+        '/ddtdt113.github.io/assets/img/favicons/favicon-32x32.png',
+        '/ddtdt113.github.io/assets/img/favicons/favicon.ico',
+        '/ddtdt113.github.io/assets/img/favicons/mstile-150x150.png',
+        '/ddtdt113.github.io/assets/img/favicons/safari-pinned-tab.svg',
+        '/ddtdt113.github.io/assets/img/favicons/site.webmanifest',
+        '/ddtdt113.github.io/assets/js/dist/categories.min.js',
+        '/ddtdt113.github.io/assets/js/dist/commons.min.js',
+        '/ddtdt113.github.io/assets/js/dist/home.min.js',
+        '/ddtdt113.github.io/assets/js/dist/misc.min.js',
+        '/ddtdt113.github.io/assets/js/dist/page.min.js',
+        '/ddtdt113.github.io/assets/js/dist/post.min.js',
+        '/ddtdt113.github.io/assets/js/dist/pvreport.min.js'
 ];
 
 /* The request url with below domain will be cached */
 const allowedDomains = [
-    {% if site.google_analytics.id != empty and site.google_analytics.id %}
-        'www.googletagmanager.com',
-        'www.google-analytics.com',
-    {% endif %}
+    
 
-    '{{ site.url | split: "//" | last }}',
+    'localhost:4000',
 
-    {% if site.img_cdn contains '//' and site.img_cdn %}
-        '{{ site.img_cdn | split: '//' | last | split: '/' | first }}',
-    {% endif %}
+    
+        'chirpy-img.netlify.app',
+    
 
     'fonts.gstatic.com',
     'fonts.googleapis.com',
@@ -48,7 +60,6 @@ const allowedDomains = [
 
 /* Requests that include the following path will be banned */
 const denyUrls = [
-    {% if site.google_analytics.pv.cache_path %}
-        '{{ site.google_analytics.pv.cache_path | absolute_url }}'
-    {% endif %}
+    
 ];
+
